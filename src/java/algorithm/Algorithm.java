9409b79312f5c8d91106841aaade000477e854d0
Nicholas Shaw <nicholas.shaw@stonybrook.edu>
2018-11-28 22:12:35 -0500

    Added properties

--- src/java/algorithm/Algorithm.java
+++ src/java/algorithm/Algorithm.java
@@ -1,30 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package algorithm;
 
 import java.util.Stack;
 
 import mapObjects.State;
 
-/**
- *
- * @author spitlord
- */
 public abstract class Algorithm {
-    
- protected double currentObjectiveValue;
- protected ObjectiveFunction objectiveFunction;
- protected State currentState;
- protected int noImprovement;
- protected static int noImprovementTolerance;
- protected Stack<Move> moves;
-    
- 
- abstract public void run();
- 
- abstract protected boolean checkTerimanationConditions();
- 
+
+	protected double currentObjectiveValue;
+	protected ObjectiveFunction objectiveFunction;
+	protected State currentState;
+	protected Stack<Move> moves;
+
+	abstract public void run();
+
+	abstract protected boolean checkTerimanationConditions();
+
 }
