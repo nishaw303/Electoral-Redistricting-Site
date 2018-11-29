45312537967c321d9f515430bf54082a43627e75
Nicholas Shaw <nicholas.shaw@stonybrook.edu>
2018-11-29 02:47:05 -0500

    Cleaned up servlets

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -8,41 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author spitlord
- */
-
-//@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
-    
 
+	private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-        
-        HttpSession session = request.getSession();
-        session.setMaxInactiveInterval(Integer.MAX_VALUE);
-        
-        // getting the info from the submitted form
-        String username = request.getParameter("username");
-        session.setAttribute("username", username);
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-        // poopy poop
-        // k
-        // redirecting to the map page
-        response.sendRedirect("/election/map.jsp");
-        
-        // suka suka
-      
-       
-    }
+		HttpSession session = request.getSession();
+		session.setMaxInactiveInterval(Integer.MAX_VALUE);
+		String username = request.getParameter("username");
+		session.setAttribute("username", username);
+		response.sendRedirect("/election/map.jsp");
+	}
 
 }
--- src/java/servlets/RegisterServlet.java
+++ src/java/servlets/RegisterServlet.java
@@ -1,98 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author spitlord
- */
 public class RegisterServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    }
+	private static final long serialVersionUID = 1L;
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-    
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	}
 
+	private void register(HttpServletRequest request) {
 
-      
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
 
-       
-
-    }
-    
-    
-    private void register(HttpServletRequest request) {
-        
-        String firstName  = request.getParameter("firstname");
-        String lastName  = request.getParameter("lastname");
-        String email = request.getParameter("email");
-        String password  = request.getParameter("password");
-        
-        try {
-
-            //loading drivers for mysql
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-
-            //creating connection with the database 
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/student?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "divny", "password");
-
-            PreparedStatement ps = con.prepareStatement("insert into Student values(?,?,?)");
-
-//            ps.setString(1, name);
-//            ps.setString(2, email);
-//            ps.setString(3, pass);
-            int i = ps.executeUpdate();
-        } catch (Exception e) {}
-    }
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/student?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"divny", "password");
+			PreparedStatement ps = con.prepareStatement("insert into Student values(?,?,?)");
+			ps.setString(1, firstName + lastName);
+			ps.setString(2, email);
+			ps.setString(3, password);
+			int i = ps.executeUpdate();
+		} catch (Exception e) {
+		}
+	}
 
 }
