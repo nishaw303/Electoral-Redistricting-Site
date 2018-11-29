f6e3bd76e3e01d6278f8b9ffe0c51ae9213d1579
Nadya Kadayifci <nadkad@Nadyas-MacBook-Pro.local>
2018-11-29 13:14:09 -0500

    Remove author tag

--- src/java/manager/Preprocessing.java
+++ src/java/manager/Preprocessing.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package manager;
 
 import com.google.gson.Gson;
@@ -14,10 +9,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import mapObjects.State;
 
-/**
- *
- * @author spitlord
- */
 public class Preprocessing {
 
     public static State initializeStateFromFile(StateName name, String filePath) {
