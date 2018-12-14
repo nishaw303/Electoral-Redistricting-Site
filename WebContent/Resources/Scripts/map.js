dd0684c167a81ad3310d76735036cfdd9e4dcccc
Nadya Kadayifci <nadkad@Nadyas-MacBook-Pro.local>
2018-12-14 02:51:24 -0500

    update map js functions

--- WebContent/Resources/Scripts/map.js
+++ WebContent/Resources/Scripts/map.js
@@ -1,8 +1,16 @@
+// *** GLOBAL VARIABLES ***
+var colors = ['#A2A79E', '#2B2D42', '#8D99AE', '#C5D5E8', '#476C9B',
+	'#EAE8FF','#DFDCE1', '#ADACB5', '#2D3142', '#B0D7FF'];
+var currYear;
+var selectedState = null;
+var currentLayer;
+var map;
+var usaLayer;
+var orLayer;
+var ohLayer;
+var maLayer;
 
 // **** MAPS API FUNCTIONS ******
-
-var map;
-var currentLayer;
 function initialize() {
     map = initMap();
     initSearchBox(map);
@@ -16,56 +24,59 @@ function initMap() {
 	});
 
     // creating a layer
-    var usaLayer = new google.maps.Data();
+    usaLayer = new google.maps.Data();
     usaLayer.loadGeoJson(properties.usaLayer);
     usaLayer.setMap(map);
 
-//        styling map:
-        usaLayer.setStyle(
-            {fillColor: 'pink',
-             opacity: 0.3}   // styling object, key-value
-        );
+    // styling map:
+    usaLayer.setStyle({
+    	fillColor: 'silver',
+        opacity: 0.3
+    })
 
-
-    // styling a particular feature:
-//    usaLayer.addListener('mouseover', function(event) {
-//        selected = event.feature;
-//        usaLayer.setStyle(function(feature) {
-//            if(feature == selected) {
-//                usaLayer.overrideStyle(selected, {fillColor: 'green'});
-//            }
-//            else {
-//                usaLayer.overrideStyle(feature, {fillColor: 'red'});
-//            }
-//        });
-//    });
+    // sets selected state
+	usaLayer.addListener('click', function(event) {
+		var state = event.feature;
+		setSelected(state);
+	
+	});
     
-  
+    // styling a particular feature:
+    usaLayer.addListener('mouseover', function(event) {
+        selected = event.feature;
+        usaLayer.setStyle(function(feature) {
+            if(feature == selected) {
+                usaLayer.overrideStyle(selected, {fillColor: 'green'});
+            }
+        });
+    });
 
-    var orPrecinctLayer = new google.maps.Data();
-    orPrecinctLayer.loadGeoJson(properties.orPrecinctsLayer);
-    orPrecinctLayer.setMap(map);
-    currentLayer = orPrecinctLayer;
+    usaLayer.addListener('mouseout', function(event) {
+    	selected = event.feature;
+    	usaLayer.setStyle(function(feature) {
+    		if (feature == selected) {
+    			usaLayer.revertStyle();
+    		}
+    	})
+    });
 
+    orLayer = new google.maps.Data();
+    orLayer.loadGeoJson(properties.orPrecinctsLayer);
 
-    var orDistrictsLayer = new google.maps.Data();
-    orDistrictsLayer.loadGeoJson(properties.orDistrictsLayer);
-//    orDistrictsLayer.setMap(map)
+    ohLayer = new google.maps.Data();
+    ohLayer.loadGeoJson(properties.ohPrecinctsLayer);
 
-    var ohPrecinctsLayer = new google.maps.Data();
-    ohPrecinctsLayer.loadGeoJson(properties.ohPrecinctsLayer);
-
-    var maPrecinctsLayer = new google.maps.Data();
-    maPrecinctsLayer.loadGeoJson(properties.maPrecinctsLayer);
-
+    maLayer = new google.maps.Data();
+    maLayer.loadGeoJson(properties.maPrecinctsLayer);
 
     google.maps.event.addListener(map, 'click', function(event) {
-    console.log(event.latLng.lat(),event.latLng.lng());
-});
+    	console.log(event.latLng.lat(),event.latLng.lng());
+    });
 
-    return map;
+	return map;
 
 }
+
 
 // ** PREFERENCE MANAGEMENT **
 function saveWeights() {
@@ -114,39 +125,131 @@ function load(filename) {
   //}
 }
 
-//function delete(filename) {
-//} can't call function delete its a js keyword
+function deleteFile(filename) {
 
-function selectState(State) {
-  selectedState = state;
-  updateCurrLayer();
+}
+
+function setSelected(state) {
+	if (state != selectedState) {
+		if (selectedState != null) {
+			selectedState.revertStyle();
+		}
+		selectedState = state.getProperty('name'); // setting global variable
+		map.data.overrideStyle(state, {strokeWeight: 3}); // adds border to selected state
+		setCurrentLayer(selectedState);
+	}
   // change color, border, zoom, pan, etc of selected state as necessary
 }
-// *** ALGORITHM UPDATE FUNCTIONS ***
-var currLayer;
-var selectedState;
 
-function updateCurrLayer() {
-  currLayer = selectedState;
+function setCurrentLayer(selectedState) {
+  if (selectedState == "Oregon") {
+	  currentLayer = orLayer;
+	  orLayer.setMap(map);
+  } else if (selectedState == "Ohio") {
+	  currentLayer = ohLayer;
+	  ohLayer.setMap(map);
+  } else if (selectedState == "Massachussetts") {
+	  currentLayer = maLayer;
+	  maLayer.setMap(map);
+  }
+
+  // info popup upon clicking a precinct
+  var contentString = '<div id="content">'+
+  '<div id="siteNotice">'+
+  '</div>'+
+  '<h1 id="firstHeading" class="firstHeading">2016</h1>'+
+  '<hr></hr>'+
+  '<div id="bodyContent">'+
+  '<p><b>Democrat:</b> 50% <span class="democrat box"></span></p>'+
+  '<p><b>Republican:</b> 50% <span class="republican box"></span></p>'+
+  '<hr></hr>'+
+  '<p><b>Black:</b> 30% <span class="black box"></span></p>'+
+  '<p><b>White:</b> 55% <span class="white box"></span></p>'+
+  '<p><b>Other:</b> 5% <span class="other box"></span></p>'+
+  '</div>'+
+  '</div>';
+
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString,
+		maxWidth: 200
+	});
+
+	currentLayer.addListener('mouseover', function(event) {
+		var precinct = feature.event;
+		//var contentString = getInfo(precinct);
+		//infowindow.setContent(contentString);
+		infowindow.open(map, precinct);
+	});
+
+	currentLayer.addListener('mouseout', function(event) {
+		  var precinct = feature.event;
+		  infowindow.close(map, precinct);
+	});
 }
 
-function getMoves() {
+function getInfo(precinct) {
+	var contentString = '<div id="content">'+
+	  '<div id="siteNotice">'+
+	  '</div>'+
+	  '<h1 id="firstHeading" class="firstHeading"> ' + currYear + '</h1>'+
+	  '<hr></hr>'+
+	  '<div id="bodyContent">'+
+	  '<p><b>Democrat:</b>' + feature.VotingData.democrat + '%<span class="democrat box"></span></p>'+
+	  '<p><b>Republican:</b>' + feature.VotingData.republican + '%<span class="republican box"></span></p>'+
+	  '<hr></hr>'+
+	  '<p><b>Black:</b>' + feature.DemographicData.black + '%<span class="black box"></span></p>'+
+	  '<p><b>White:</b>' + feature.DemographicData.white + '%<span class="white box"></span></p>'+
+	  '<p><b>Other:</b>' + feature.DemographicData.other + '%<span class="other box"></span></p>'+
+	  '</div>'+
+	  '</div>';
+
+	return contentString;
+}
+
+//*** ALGORITHM UPDATE FUNCTIONS ***
+function displayMoves() {
+	var moves = getMoves();
+	if (moves != -1) {
+		moves.forEach(move => {
+			showMovePrecinct(move);
+		});
+	}
+}
+
+function getMoves(numMoves) {
   var moves = -1;
   // get 10 from backend
+  function request() {
+      $.ajax({
+          url: 'updating',
+          type: 'GET',
+          dataType: 'json',
+          success: function (jsonObject) {
+              moves = jsonObject;
+          },
+          error: function (error) {
+              alert("Moves still in progress");
+          }
+      });
+   }
+
+  // var i = 0;
+  // var interval = setInterval(request, 250);
+  request();
   return moves;
 }
 
 function showMovePrecinct(move) {
-  var src = move.sourceDistrict;
-  var dest = move.destinationDistrict;
+  var srcID = move.sourceDistrict;
+  var destID = move.destinationDistrict;
   var precinctID = move.precinct;
 
-  // currLayer is a layer which has features, which correspond to precinct ids.
+  var feature = currentLayer.getFeatureById(precinctID);
+  if (destID <= colors.length) {
+	  var newColor = colors[destID];
+  } else {
+	  var newColor = colors[destID - (destID-colors.length)];
+  }
+  currentLayer.setStyle(feature, {fillColor: newColor});
 
-} // update the color of one precinct
-
-
-function chooseColors(number) {
-    var colors = []
-    return colors;
 }
