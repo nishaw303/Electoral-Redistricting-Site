6e9065d8393bea363987eef14a7bad9dec5a93ab
spitlord <spitlord@marks-mbp.home>
2018-12-14 09:45:10 -0500

    asldjgk

--- WebContent/Resources/Scripts/properties.js
+++ WebContent/Resources/Scripts/properties.js
@@ -1,8 +1,8 @@
 var properties = {
     usaLayer: 'https://raw.githubusercontent.com/spitlord/fileHost/master/us-states.json',
-    orPrecinctsLayer: 'https://raw.githubusercontent.com/spitlord/fileHost/master/oregonPrecincts.json',
-    ohPrecinctsLayer: 'https://raw.githubusercontent.com/spitlord/election/master/WebContent/Resources/Layers/Ohio.json',
-    maPrecinctsLayer: 'https://raw.githubusercontent.com/spitlord/election/master/WebContent/Resources/Layers/Massachusetts.json',
+    orPrecinctsLayer: 'https://raw.githubusercontent.com/spitlord/fileHost/master/Oregon.json',
+    ohPrecinctsLayer: 'https://github.com/spitlord/fileHost/blob/master/Ohio.json',
+    maPrecinctsLayer: 'https://github.com/spitlord/fileHost/blob/master/Masachusetts.json',
     orDistrictsLayer: 'https://raw.githubusercontent.com/spitlord/fileHost/master/oregonDistricts.json',
     ohDistrictsLayer: 'https://raw.githubusercontent.com/spitlord/fileHost/master/ohioDistricts.json',
     maDistrictsLayer: 'https://raw.githubusercontent.com/spitlord/fileHost/master/massachusettsDistricts.json'
